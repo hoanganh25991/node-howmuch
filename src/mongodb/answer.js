486909ef8fd54fa076f1f5f6a342a5f787bf8548
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-11-16 15:18:24 +0700

    Summary & test #1

--- src/mongodb/answer.js
+++ src/mongodb/answer.js
@@ -1,5 +1,10 @@
 import mongoose from "mongoose"
 
+const _ = console.log
+const ratio = 1.35
+export const PLATFORM = "PLATFORM"
+export const MULTIPLY = "MULTIPLY"
+
 export const getAll = () => {
   const Answer = mongoose.model("Answer")
   return Answer.find({})
@@ -22,16 +27,38 @@ export const updateAnswerSession = data => {
     .catch(err => err)
 }
 
+export const computeSummary = answers => {
+  // Find platform answer
+  const platormAnsArr = answers.filter(ans => ans.type === PLATFORM)
+  const multiply = platormAnsArr.reduce((carry, ans) => ({ ...carry, ...ans }), {})
+  _(multiply)
+
+  const multiplyAnsArr = answers.filter(ans => ans.type === MULTIPLY)
+  const lastMultiply = multiplyAnsArr.reduce((carry, multiply) => {
+    const { ios: li = 1, android: la = 1, web: lw = 1 } = carry
+    const { ios: ni = 1, android: na = 1, web: nw = 1 } = multiply
+    return { ios: li * ni, android: la * na, web: lw * nw }
+  }, {})
+
+  const multiplyTotal = Object.keys(lastMultiply).reduce((a, b) => a + b)
+
+  // Compute summary
+  const normalAnsArr = answers.filter(ans => ans.type !== PLATFORM && ans.type !== MULTIPLY)
+
+  // Do compute logic with answer
+  return normalAnsArr.reduce((carry, ans) => {
+    const { pay, fixed_pay } = ans
+    return carry + fixed_pay + pay * multiplyTotal
+  }, 0)
+}
+
 export const getSummary = async sessionId => {
   const Answer = mongoose.model("Answer")
   const answer = await Answer.findOne({ sessionId })
     .exec()
     .catch(err => err)
 
-  // Do compute logic with answer
-
-  return {
-    summary: Math.floor(Math.random() * 10000),
-    ratio: 1.35
-  }
+  const answers = (answer && answer.answers) || []
+  const summary = computeSummary(answers)
+  return { summary, ratio }
 }
